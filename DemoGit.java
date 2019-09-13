8a4677aca4776cc7913845983a628abb5de4ec3b
Chad <chad.spector@mail.mcgill.ca>
2019-09-13 13:11:51 -0400

    first commit

---
+++ DemoGit.java
@@ -0,0 +1,14 @@
+public class DemoGit{
+
+	public static void main(String[] args) {
+		System.out.println("Hello Class. This is a demo program");
+		int abc = division_method(32,4);
+		System.out.println(abc);
+
+	}
+	
+	private static int division_method(int i, int j){
+		return i/j;
+	}
+}
+
